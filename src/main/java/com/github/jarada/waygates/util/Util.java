8d13f2efa7e9f5daddbb949c0191e9df1255aaa4
Jarada <jarada0077@mac.com>
2022-09-18 16:20:12 +0100

    Update dependencies to latest versions; add Apache Commons
    1.19 removed support for Apache Commons, so we add in the latest supported version

--- src/main/java/com/github/jarada/waygates/util/Util.java
+++ src/main/java/com/github/jarada/waygates/util/Util.java
@@ -1,6 +1,6 @@
 package com.github.jarada.waygates.util;
 
-import org.apache.commons.lang.WordUtils;
+import org.apache.commons.text.WordUtils;
 import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
