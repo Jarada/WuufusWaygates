c3c8deb4a7949df67f06372749170096da2d741e
datatags <datatags@users.noreply.github.com>
2025-04-13 21:21:17 -0700

    Add handling for BlockStateMeta internal data

--- src/main/java/com/github/jarada/waygates/util/ItemStackUtil.java
+++ src/main/java/com/github/jarada/waygates/util/ItemStackUtil.java
@@ -1,7 +1,11 @@
 package com.github.jarada.waygates.util;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.block.BlockState;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockStateMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.jetbrains.annotations.Nullable;
 
@@ -35,14 +39,28 @@ public class ItemStackUtil {
 
             if (o1 == null && o2 == null)
                 continue;
-            
+
             if (o1 == null || o2 == null)
                 return false;
 
-            if (o1.getType() == Material.PLAYER_HEAD && o2.getType() == Material.PLAYER_HEAD) {
+            if (o1.getType() != o2.getType())
+                return false;
+
+            if (o1.getType() == Material.PLAYER_HEAD) {
                 // We verify the inventory on other items, player heads just cause problems
                 // So providing the two items are player heads that will do for us
                 continue;
+            }
+
+            if (o1.getItemMeta() instanceof BlockStateMeta && o2.getItemMeta() instanceof BlockStateMeta) {
+                // Block state meta spontaneously creates "internal" data that causes it to not be equal.
+                // So, we create placeholders for the state part of the meta and compare them.
+                BlockState state = Bukkit.createBlockData(o1.getType()).createBlockState();
+                BlockStateMeta m1 = (BlockStateMeta) o1.getItemMeta();
+                BlockStateMeta m2 = (BlockStateMeta) o2.getItemMeta();
+                m1.setBlockState(state);
+                m2.setBlockState(state);
+                return m1.equals(m2);
             }
 
             if (!(Objects.equals(o1, o2)))
